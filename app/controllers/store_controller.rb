d7566422afe08976baeb49ac3aca92f786939a41
Demi Raven <ravenart@demi-ravens-computer-2.local>
2009-01-31 17:38:37 -0800

    chapter 8 work, additional tests needed.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,4 +3,32 @@ class StoreController < ApplicationController
     @products = Product.find_products_for_sale
   end
 
+  def add_to_cart
+    begin
+      product = Product.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error("Attempt to access invalid product #{params[:id]}")
+      redirect_to_index("Invalid product")
+    else
+      @cart = find_cart
+      @cart.add_product(product)
+    end 
+  end
+
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index("Your cart is currently empty")
+  end
+
+  private
+
+  def find_cart
+    session[:cart] ||= Cart.new
+  end
+
+  def redirect_to_index(msg)
+    flash[:notice]
+    redirect_to :action => 'index'
+  end
+
 end
