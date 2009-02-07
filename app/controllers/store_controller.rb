ae13856be081284895dc7db83c082580ef8cde09
Demi <kirklandnokia@kirkland-nokias-macbook-pro.local>
2009-02-07 11:45:14 -0800

    added in AJAX add to cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
+    @cart = find_cart
   end
 
   def add_to_cart
@@ -12,6 +13,9 @@ class StoreController < ApplicationController
     else
       @cart = find_cart
       @cart.add_product(product)
+      respond_to do |format|
+        format.js
+      end   
     end 
   end
 
@@ -26,8 +30,8 @@ class StoreController < ApplicationController
     session[:cart] ||= Cart.new
   end
 
-  def redirect_to_index(msg)
-    flash[:notice]
+  def redirect_to_index(msg = nil)
+    flash[:notice] = msg if msg
     redirect_to :action => 'index'
   end
 
