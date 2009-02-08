55bfc8185c4c5d97e4c6a531d47ce63c9fa4c5a4
Demi <kirklandnokia@kirkland-nokias-macbook-pro.local>
2009-02-08 10:36:33 -0800

    completed week 3

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -6,27 +6,24 @@ class StoreControllerTest < ActionController::TestCase
     assert session[:cart]
   end
 
-  #TODO: COMPLETE
   test "adding to cart works" do
-   xhr :post, :add_to_cart, {:id => :one}, nil, nil
-   
-   # assert cart = assigns[:cart]
-   # assert_equal 1, cart.items.length
-   
-    #this should work, but is failing for some reason.  In a web trace it is fine.  Instead I receive a 302 :redirect 
-    #assert_response :success
-
-    assert_response :redirect
+   get :index
+   assert cart = session[:cart]
+   assert_equal 0, cart.items.length   
+   xhr :post, :add_to_cart, {:id => products(:one).id}
+   assert_response :success
+   assert_equal 1, cart.items.length
   end
 
-  #TODO: COMPLETE
   test "clearing cart works" do
-    #TODO once xhr issues sorted out add to cart here
- 
+    get :index
+    assert cart = session[:cart]
+    xhr :post, :add_to_cart, {:id => products(:one).id}
+    assert_equal 1, cart.items.length
     post :empty_cart
-    assert_response :redirect
-    assert !assigns[:cart]
-    # assert_tag :tag => 'div', :attributes => {:id => 'cart', :style => 'display: none'} 
+    assert_redirected_to :controller => 'store', :action => :index
+    get :index
+    assert_tag :tag => 'div', :attributes => {:id => 'cart', :style => 'display: none'} 
   end
 
   test "redirect on bad index" do
@@ -43,8 +40,7 @@ class StoreControllerTest < ActionController::TestCase
     Product.find_products_for_sale.each {|p|
       assert_tag :tag =>'h3', :content => p.title
       assert_tag :tag =>'img', :attributes => {:src => '/images/' + p.image_url}
-      # QUESTION: is sprintf or format better?
-      assert_match /#{format("%.2f", p.price / 100.0)}/, @response.body  
+      assert_match /#{"%.2f" %  (p.price / 100.0)}/, @response.body  
     }
   end
 end
