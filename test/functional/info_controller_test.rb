6e18f0807ebf2e6a8642b6f13eb1c2ae045d86f5
Demi <kirklandnokia@kirkland-nokias-macbook-pro.local>
2009-02-16 22:03:43 -0800

    completed through p189 including tests

--- test/functional/info_controller_test.rb
+++ test/functional/info_controller_test.rb
@@ -1,10 +1,21 @@
 require 'test_helper'
 
 class InfoControllerTest < ActionController::TestCase
-  test "can get xml info on order" do
+  def setup
     li = LineItem.new(:product_id => products(:one).id, :order_id => orders(:one).id, :quantity => 1, :total_price => 1000)
     li.save!
+  end  
+
+
+  test "can get xml info on order" do
+    @request.env['HTTP_ACCEPT'] = 'application/xml'
     get :who_bought, :id => products(:one).id
     assert_tag :tag => 'name', :content => 'productbuyer'
+  end
+
+  test "can get html info on order" do
+    get :who_bought, :id => products(:one).id
+    assert_match /!DOCTYPE/, @response.body
+    assert_match /productbuyer/, @response.body  
   end
 end
