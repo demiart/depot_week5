df9730bcf6d1afd3f6e883cd7b5ef4958dda5bcf
Demi <kirklandnokia@kirkland-nokias-macbook-pro.local>
2009-02-21 16:48:26 -0800

    added i18n for title and side

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,6 +4,7 @@
 class ApplicationController < ActionController::Base
   layout "store"
   before_filter :authorize, :except => :login
+  before_filter :set_locale
   helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
@@ -16,6 +17,26 @@ class ApplicationController < ActionController::Base
   # filter_parameter_logging :password
 
 protected
+  def set_locale
+    begin
+      session[:locale] = params[:locale]  if params[:locale]
+      I18n.locale = session[:locale] || I18n.default_locale
+
+      locale_path = "#{LOCALES_DIRECTORY}#{I18n.locale}.yml"
+
+      unless I18n.load_path.include? locale_path  
+        I18n.load_path << locale_path 
+        I18n.backend.send(:init_translations) 
+      end 
+
+    rescue Exception => err 
+      logger.error err 
+      flash.now[:notice] = "#{I18n.locale} translation not available" 
+      I18n.load_path -= [locale_path] 
+      I18n.locale = session[:locale] = I18n.default_locale 
+    end
+  end 
+
   def authorize
     unless User.find_by_id(session[:user_id])
       session[:user_id] = nil
